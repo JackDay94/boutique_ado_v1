040aa871df43e35687574c57d9bc0d10131647a7
JackDay94 <Jack94day@gmail.com>
2023-01-19 17:11:39 +0000

    Add adjust bag and remove bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -7,6 +7,55 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity  # Find item id and item size and set its quantity
+        else:
+            del bag[item_id]['items_by_size'][size]  # Remove item if quantity less than 1
+            if not bag[item_id]['items_by_size']: 
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity  # Add item with item id
+        else:
+            bag.pop(item_id)  # Remove item with item id
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]  # Delete item with a specific size
+            if not bag[item_id]['items_by_size']: 
+                bag.pop(item_id)  # Remove entire item_id if only size in bag
+        else:
+            bag.pop(item_id)  # Remove item with item id
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
 
 
 def add_to_bag(request, item_id):
