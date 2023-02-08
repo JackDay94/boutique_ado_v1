ef4225763aea6684487f4f4970abc387a3fb42fd
JackDay94 <Jack94day@gmail.com>
2023-02-08 18:25:11 +0000

    Update webhook handler to handle profiles

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,6 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
@@ -118,82 +117,3 @@ form.addEventListener('submit', function(ev) {
     })
 });
 
-
-// // Handle form submit
-// var form = document.getElementById('payment-form');
-
-// form.addEventListener('submit', function(ev) {
-//     ev.preventDefault();
-//     // Disable card element and submit button to prevent multiple submissions
-//     card.update({ 'disabled': true});
-//     $('#submit-button').attr('disabled', true);
-//     $('#payment-form').fadeToggle(100);
-//     $('#loading-overlay').fadeToggle(100);
-
-//     var saveInfo = Boolean($('#id-save-info').attr('checked'));
-//     // From using {% csrf_token %} in the form
-//     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-//     var postData = {
-//         'csrfmiddlewaretoken': csrfToken,
-//         'client_secret': clientSecret,
-//         'save_info': saveInfo,
-//     }
-//     var url = '/checkout/cache_checkout_data/';
-
-//     $.post(url, postData).done(function() {
-//         // Call confirmCardPayment method and provide the card to stripe
-//         stripe.confirmCardPayment(clientSecret, {
-//             payment_method: {
-//                 card: card,
-//                 billing_details: {
-//                     name: $.trim(form.full_name.value),
-//                     phone: $.trim(form.phone_number.value),
-//                     email: $.trim(form.email.value),
-//                     address:{
-//                         line1: $.trim(form.street_address1.value),
-//                         line2: $.trim(form.street_address2.value),
-//                         city: $.trim(form.town_or_city.value),
-//                         country: $.trim(form.country.value),
-//                         state: $.trim(form.county.value),
-//                     }
-//                 }
-//             },
-//             shipping: {
-//                 name: $.trim(form.full_name.value),
-//                 phone: $.trim(form.phone_number.value),
-//                 address:{
-//                     line1: $.trim(form.street_address1.value),
-//                     line2: $.trim(form.street_address2.value),
-//                     city: $.trim(form.town_or_city.value),
-//                     country: $.trim(form.country.value),
-//                     postal_code: $.trim(form.postcode.value),
-//                     state: $.trim(form.county.value),
-//                 }
-//             },
-//         // Execute this function on the result
-//         }).then(function(result) {
-//             // If there is an error show this message and renable the card element and submit button
-//             if (result.error) {
-//                 var errorDiv = document.getElementById('card-errors');
-//                 var html = `
-//                     <span class="icon" role="alert">
-//                     <i class="fas fa-times"></i>
-//                     </span>
-//                     <span>${result.error.message}</span>`;
-//                 $(errorDiv).html(html);
-//                 $('#payment-form').fadeToggle(100);
-//                 $('#loading-overlay').fadeToggle(100);
-//                 card.update({ 'disabled': false});
-//                 $('#submit-button').attr('disabled', false);
-//             // If the status of the payment intent returns succeeded then submit the form    
-//             } else {
-//                 if (result.paymentIntent.status === 'succeeded') {
-//                     form.submit();
-//                 }
-//             }
-//         });
-//     }).fail(function() {
-//         // Reload the page, the error will be in django messages
-//         location.reload();
-//     })  
-// });
